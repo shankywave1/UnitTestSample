3b840aa9fd6773aa36d1247081461513fb5cbdb5
Shashank Kaushik <kaushik.shashank1@gmail.com>
2024-06-22 14:11:22 +0530

    Add destination

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # xcodebuild build -target UnitTestSample
-xcodebuild build -target UnitTestSample CODE_SIGNING_ALLOWED='NO'
+xcodebuild build -target UnitTestSample -destination "platform=iOS Simulator,name=iPhone 14" CODE_SIGNING_ALLOWED='NO'
 
 # xcodebuild test -target UnitTestSample -scheme UnitTestSample
-xcodebuild test -target UnitTestSample -scheme UnitTestSample CODE_SIGNING_ALLOWED='NO'
+xcodebuild test -target UnitTestSample -scheme UnitTestSample -destination "platform=iOS Simulator,name=iPhone 14" CODE_SIGNING_ALLOWED='NO'
 
